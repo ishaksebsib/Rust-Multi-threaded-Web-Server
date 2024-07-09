6e928d277f9049d67267a3b3a9f879c524570c77
ishak <ishaksebsib@gmail.com>
2024-07-09 16:44:45 +0300

    ENH: very basic tcp response for http request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-        println!("Connection established!");
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.read(&mut buffer).unwrap();
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
