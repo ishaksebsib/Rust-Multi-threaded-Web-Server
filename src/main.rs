7634356cf594972b089c28a54e95464137d239f9
ishak <ishaksebsib@gmail.com>
2024-07-09 17:28:02 +0300

    ENH: add very simple routing that returns home page to / reqeust and notfound pagae to all others: and made some code refactoring

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,28 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let mut file = File::open("hello.html").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    // check is the request is to main page (url+/) else response with 404
+    let get = b"GET / HTTP/1.1\r\n";
+    let response = if buffer.starts_with(get) {
+        create_response("hello.html", "200 OK")
+    } else {
+        create_response("404.html", "404 Not Found")
+    };
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
+
+fn create_response(file_path: &str, status: &str) -> String {
+    let mut page = File::open(&file_path).unwrap();
+    let mut page_contents = String::new();
+    page.read_to_string(&mut page_contents).unwrap();
+
+    format!(
+        "HTTP/1.1 {}\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n{}",
+        status,
+        page_contents.len(),
+        page_contents
+    )
 }
